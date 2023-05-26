fe20133937bc1a8838cebe9f0fd4df4be3e98942
Apple03244 <apple03244@gmail.com>
2023-05-26 15:39:46 +0900

    23.5.26

---
+++ python 기존공부/프로그래스 문제/난이도_1/039_비밀지도.py
@@ -0,0 +1,18 @@
+import numpy as np
+def solution(n, arr1, arr2):
+    def chagebase(x,y): #y:base
+        result=[]
+        while x>=y:
+
+
+
+a,sum=0,0
+while a<=1000:
+    a+=1
+    if a%2:
+        continue
+    sum+=a
+
+a=[i for i in range(1001)]
+
+[i for i in]
