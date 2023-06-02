fbdca9dbaf5f1b1cd9b89ab07537d00b72c5cb98
Apple03244 <apple03244@gmail.com>
2023-06-02 18:02:40 +0900

    23.5.26

--- python 기존공부/프로그래스 문제/난이도_1/051_소수찾기.py
+++ python 기존공부/프로그래스 문제/난이도_1/051_소수찾기.py
@@ -12,8 +12,46 @@
 
 #효율성이 안나옴
 def solution(x):
-    result=list(range(1,x+1))
-    def cut(x,y):
-        if x==1:
-            pass
+    prime = lambda x: 1 if all([bool(x % i) for i in range(2, x)]) else 0
+    ls=list(range(x,1,-1))
+    result=0
+    for x in ls:
+        result+=prime(x)
+    return result
+#
+def solution(n):
+    ls=list(range(n,1,-1))
+    result=[]
+    for i in range(len(ls)):
+        for j in range(i+1,len(ls)):
+            if ls[i]%ls[j]==0:
+                result.append(ls[i])
+                break
+    return n-1-len(result)
 
+solution(10)
+import math
+dir(math)
+def solution(n):
+    prime = lambda x: 1 if all([x % i for i in range(2, x)]) else 0
+    return sum(map(prime,range(2,n+1)))
+
+def solution(n):
+    #filter 써보자
+    m=2
+    numlist=list(range(2,n+1))
+    result=[]
+    while True:
+        result.append(m)
+        numlist=list(filter(lambda x:x%m!=0,numlist))
+        print(numlist,m,result)
+        if not numlist:
+            break
+        else:
+            m=numlist[0]
+    return len(result)
+)solution(5)
+
+
+a=[1,2,3,4]
+a.de(10)
